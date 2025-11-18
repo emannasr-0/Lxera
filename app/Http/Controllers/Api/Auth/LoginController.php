542016619b58646d263f9c90e030aebee71dc1b8
Roaaanasco <roaa.a@anascogroup.com>
2025-11-18 11:46:34 +0200

    login

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -114,7 +114,6 @@ class LoginController extends Controller
             "id" => $user->id,
             "full_name" => $user->full_name,
             "role_name" => $user->role_name,
-            "role_id" => $user->role_id,
             "user_code" => $user->user_code,
             "mobile" => $user->mobile,
             "email" => $user->email,
